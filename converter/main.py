0b14410fcbebac49cd4d281b14755de849274cc8
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-08-02 01:49:53 +0100

    Implemented basic error checking and conversion success/fail count

--- converter/main.py
+++ converter/main.py
@@ -8,68 +8,91 @@ print("Playlist Converter")
 
 main_directory = "E:\A\Music"
 music_library = "E:\A\Music\Library"
+playlist_in = ""
+playlist_name = ""
 
-#playlist_directory = "E:\A\Music\_python\Playlists\Forgetable"
-#playlist_in = "E:\A\Music\_python\Playlists\Forgetable\playlist.m3u"
 
-playlist_in = input("Please enter the absolute path of the playlist file\n")
-while not os.path.exists(playlist_in):
-    playlist_in = input("Playlist file does not exist, please try again\n")
+def requestPlaylist():
+    playlist = input("Please enter the absolute path of the playlist file\n")
+    while not os.path.exists(playlist):
+        if playlist.upper() == "EXIT":
+            print("Program shutdown")
+            sys.exit(0)
+        playlist = input("Playlist file does not exist, please try again\n")
+    return playlist;
 
-playlist_directory = os.path.dirname(playlist_in)
+#playlist_directory = os.path.dirname(playlist_in)
 
-name = input("Please enter a name for the converted playlist: ")
-while len(name) < 1:
-    name = input("Invalid name. Please enter a valid name: ")
+def requestOutputName():
+    name = input("Please enter a name for the converted playlist: ")
+    while len(name) < 1:
+        name = input("Invalid name. Please enter a valid name: ")
 
-# Reverse the filename to check whether it has a .m3u extension
-# If length is not more than 4 then it physically cannot have a .m3u extension
-if len(name) > 4:
-    reversed = name[::-1]
-    reversed = reversed[0:4]
-    reversed = reversed[::-1]
+    # Reverse the filename to check whether it has a .m3u extension
+    # If length is not more than 4 then it physically cannot have a .m3u extension
+    reversed = ""
+    if len(name) > 4:
+        reversed = name[::-1]
+        reversed = reversed[0:4]
+        reversed = reversed[::-1]
 
-# If the filename does not include .m3u then add it
-if not reversed == ".m3u":
-    name += ".m3u"
+    # If the chosen filename does not include .m3u then add it
+    if not reversed == ".m3u":
+        name += ".m3u"
+    return name;
 
-playlist_out = "E:\A\Music\_python-playlists"
+playlist_output_dir = "E:\A\Music\_python-playlists"
+
+playlist_in = requestPlaylist()
+playlist_name = requestOutputName()
 
 playlist_file = open(playlist_in, "r")
 
 songs = []
 
-index = 0;
+success_count = 0;
+fail_count = 0;
+
 for line in playlist_file:
-    #print(index," ", line)
-    index += 1
 
-    file_path = playlist_directory + "\\" + line
+    file_path = os.path.dirname(playlist_in) + "\\" + line
     file_path = file_path.rstrip()
-    print(file_path)
 
-    audiofile = eyed3.load(file_path)
-    artists = audiofile.tag.artist
-    artists = artists.split(',')
-    primary_artist = artists[0]
-    album = audiofile.tag.album
+    if os.path.exists(file_path):
+        audiofile = eyed3.load(file_path)
+        artists = ""
+        if audiofile.tag.artist is not None:
+            artists = audiofile.tag.artist
+        artists = artists.split(',')
+        primary_artist = artists[0]
+        album = ""
+        if audiofile.tag.album is not None:
+            album = audiofile.tag.album
 
-    real_location = music_library + "\\" + primary_artist + "\\" + album + "\\" + line
-    #print(real_location)
-    if len(primary_artist) > 0 and len(album) > 0:
-        songs.append(real_location)
+        real_location = music_library + "\\" + primary_artist + "\\" + album + "\\" + line
+
+        if len(primary_artist) > 0 and len(album) > 0:
+            songs.append(real_location)
+            success_count += 1
+        else:
+            print("Failed to convert: " + file_path)
+            fail_count += 1
     else:
         print("Failed to convert: " + file_path)
+        fail_count += 1
 
 playlist_file.close()
 
-full_name = playlist_out + "\\" + name
-playlist_file_out = open(full_name, "w")
+full_name = playlist_output_dir + "\\" + playlist_name
+playlist_file_out = open(full_name, "w", encoding='utf-8')
 playlist_file_out.write("# Converted with Playlist Converter by Alex McGill\n")
-playlist_file_out.write("# " + name + "\n")
+playlist_file_out.write("# " + playlist_name + "\n")
 playlist_file_out.writelines(songs)
 playlist_file_out.close()
-#for current in songs:
-    #print(current)
-    #current.rstrip()
-    #playlist_file_out.write
+
+# Display conversion results
+print(success_count, "songs successfully converted.")
+if fail_count == 1:
+    print(fail_count, "song failed to convert")
+elif fail_count > 1:
+    print(fail_count, "songs failed to convert.")
